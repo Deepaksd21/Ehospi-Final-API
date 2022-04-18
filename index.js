113e8ca41c6aad16b73a5d720e38e5430a75318a
Deepak <saudeep111@gmail.com>
2022-04-18 10:31:39 +0530

    Done

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const allRoutes = require("./routes/route");
-// const swaggerUi = require("swagger-ui-express");
-// const swaggerDocument = require("./swagger/swagger.json");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
